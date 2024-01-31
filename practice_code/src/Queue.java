9980dc1e4a0202d3dd6b87d2ce5eb8c3fd3b4f4b
Cassius Francis <cassiusclay2006@gmail.com>
2024-01-31 12:23:57 +0000

    finished the basic parts of the queue class, unsure if i need to put the shift inside or outside the class

--- practice_code/src/Queue.java
+++ practice_code/src/Queue.java
@@ -1,35 +1,62 @@
 public class Queue {
-    int FOQP = 0;
-    int EOQP = -1;
-    int Size;
-    char[] Items = new char[Size];
 
-    public Queue {
-        int FOQP = 0;
-        int EOQP = -1;
-        int Size;
-        char[] Items = new char[Size];
-        public void Enqueue ( char c){
-            Items[EOQP] = c;
-            EOQP++;
-            Size++;
-        }
-        public char Dequeue () {
-            char item = Items[FOQP];
-            FOQP++;
-            Size--;
-            return item;
-        }
-        public boolean IsFull () {
-            if ()
-        }
-        public boolean IsEmpty () {
-            if (FOQP == -1) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+/*initialising variables that will be used later*/
+
+private int FOQP = 0;
+private int EOQP = -1;
+private int Size=0;
+private int length;
+private char[] Items;
+
+public Queue(int Length) {
+
+/*this creates the empty que, asking the user to input how many positions
+they want the array to have */
+
+    this.length=Length;
+    Items = new char[length];
+
+}
+    public void Enqueue ( char c){
+
+/*sets position indicated by the end of que pointer as the users char input
+then increments the end of que pointer and the size as they both increase
+by 1*/
+
+        Items[EOQP] = c;
+        EOQP++;
+        Size++;
+    }
+    public char Dequeue () {
+
+/*creates a variable called item to store the value that's in the array at
+the front of the que ie; the first value in the array, it then increments the
+the front of que pointer by one making that value inaccessible, reduces size by
+1 and returns the dequeued character to the user */
+
+        char item = Items[FOQP];
+        FOQP++;
+        Size--;
+        return item;
+    }
+    public boolean IsFull () {
+
+/*if the length length of the array is the same as the end of que pointer + 1 then
+return que is full, else false, we add one as the array starts at a value of 0*/
+
+    return length==EOQP+1;
+    }
+    public boolean IsEmpty() {
+
+/*if front of que pointer hasnt incremented then there cant be any values in the que
+so que is empty*/
+
+        return FOQP == -1;
+
+    }
+    public int length(){
+/*returns the number of values in the que at this moment*/
+return Size;
     }
 }
 
